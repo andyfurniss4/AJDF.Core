843f5b446542645dec7a82192c3a1a0aec90173b
Andy Furniss <afurniss4@googlemail.com>
2018-01-07 14:38:35 +0000

    Added base repository interfaces and EF implementation

---
+++ DAL/DAL.Base/Model/IEntity.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DAL.Base
+{
+    public interface IEntity
+    {
+        object Id { get; set; }
+        DateTime CreatedDate { get; set; }
+        DateTime? ModifiedDate { get; set; }
+        string CreatedBy { get; set; }
+        string ModifiedBy { get; set; }
+        byte[] Version { get; set; }
+    }
+}
