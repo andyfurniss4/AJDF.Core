7db1ba9539acebc38db5d3bfd5ac0ed4f2cec24b
Andy Furniss <afurniss4@googlemail.com>
2018-01-07 16:33:15 +0000

    Refactor base repository interfaces for preferable generic repository structure

--- DAL/DAL.Base/Repository/IRepository.cs
+++ DAL/DAL.Base/Repository/IRepository.cs
@@ -1,17 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Threading.Tasks;
 
 namespace DAL.Base
 {
-    public interface IRepository : IReadOnlyRepository
+    public interface IRepository<TEntity, TKey> : IRepositoryBase<TEntity> where TEntity : IEntity
     {
-        void Create<TEntity>(TEntity entity, string createdBy = null) where TEntity : class, IEntity;
-        void Update<TEntity>(TEntity entity, string modifiedBy = null) where TEntity : class, IEntity;
-        void Delete<TEntity>(object id) where TEntity : class, IEntity;
-        void Delete<TEntity>(TEntity entity) where TEntity : class, IEntity;
-        void Save();
-        Task SaveAsync();
+        TEntity GetByKey(TKey Id);
+        TEntity GetById(Expression<Func<TEntity, bool>> searchPred);
+        bool Delete(TKey id);
     }
 }
---
+++ DAL/DAL.Base/Repository/IRepositoryBase.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace DAL.Base
+{
+    public interface IRepositoryBase<TEntity> where TEntity : IEntity
+    {
+        ICollection<TEntity> Find(Expression<Func<TEntity, bool>> where);
+        IQueryable<TEntity> Find(Expression<Func<TEntity, bool>> where, params Expression<Func<TEntity, object>>[] includeProperties);
+        ICollection<TEntity> Find(Expression<Func<TEntity, bool>> searchPredicate, int pageNumber, int pageSize);
+        ICollection<TEntity> Find(Expression<Func<TEntity, bool>> searchPredicate, string columnToOrderBy, int pageNumber, int pageSize, bool orderByDescending = false);
+
+        TEntity Get(Expression<Func<TEntity, bool>> where);
+        ICollection<TEntity> GetAll();
+        ICollection<TEntity> GetAllAsync();
+        ICollection<TEntity> GetAll(params Expression<Func<TEntity, object>>[] includeProperties);
+        ICollection<TEntity> GetAllAsync(params Expression<Func<TEntity, object>>[] includeProperties);
+
+        bool Insert(IEnumerable<TEntity> items);
+        bool Insert(TEntity item);
+
+        bool Update(IEnumerable<TEntity> items);
+        bool Update(TEntity item);
+
+        bool InsertOrUpdate(IEnumerable<TEntity> items);
+        bool InsertOrUpdate(TEntity item);
+
+        bool Delete(IEnumerable<TEntity> items);
+        bool Delete(TEntity item);
+    }
+}
