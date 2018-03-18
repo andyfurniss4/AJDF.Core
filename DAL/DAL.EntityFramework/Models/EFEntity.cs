1d3f9bb2383cc35f666784224c8f6bebea0a82de
Andy Furniss <afurniss4@googlemail.com>
2018-03-18 23:24:08 +0000

    Added EF base entity

---
+++ DAL/DAL.EntityFramework/Models/EFEntity.cs
@@ -0,0 +1,13 @@
+﻿using DAL.Base;
+using System;
+
+namespace DAL.EntityFramework.Models
+{
+    public class EFEntity : IEntity
+    {
+        public int Id { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public DateTime? ModifiedDate { get; set; }
+        public byte[] Version { get; set; }
+    }
+}
